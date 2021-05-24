68dd0db226e2165940434ccbf1b80005077e4b5b
abdullah_khan123 <sardarbhai6@gmail.com>
2021-05-25 00:09:37 +0530

    setup mongodb connection

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
 const app = require('./app');
+const mongoose = require('mongoose');
+
+const url = process.env.MONGOURI || "http://127.0.0.1:27017";
+
+mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => console.log("connected"))
+    .catch(err => console.log(err.message))
+
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, ()=>console.log(`up and running on port ${PORT}`));
+app.listen(PORT, () => console.log(`up and running on port ${PORT}`));
